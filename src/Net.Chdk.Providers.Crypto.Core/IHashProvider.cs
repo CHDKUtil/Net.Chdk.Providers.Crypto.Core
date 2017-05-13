3924cea36015e4a5d7227158f71a325165dfe291
Dmitry Shechtman <damnian@gmail.com>
2017-05-13 21:02:08 +0300

    Add GetHashString(byte[], string)

--- src/Net.Chdk.Providers.Crypto.Core/IHashProvider.cs
+++ src/Net.Chdk.Providers.Crypto.Core/IHashProvider.cs
@@ -5,5 +5,6 @@ namespace Net.Chdk.Providers.Crypto
     public interface IHashProvider
     {
         string GetHashString(Stream stream, string hashName);
+        string GetHashString(byte[] buffer, string hashName);
     }
 }
