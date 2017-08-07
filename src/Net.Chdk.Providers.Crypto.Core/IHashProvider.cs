a599235b0492f5ee4f0e36ea00d02f30bafa53a0
Dmitry Shechtman <damnian@gmail.com>
2017-08-07 21:00:25 +0300

    Remove GetHash(Stream, string)

--- src/Net.Chdk.Providers.Crypto.Core/IHashProvider.cs
+++ src/Net.Chdk.Providers.Crypto.Core/IHashProvider.cs
@@ -1,10 +1,7 @@
-﻿using System.IO;
-
-namespace Net.Chdk.Providers.Crypto
+﻿namespace Net.Chdk.Providers.Crypto
 {
     public interface IHashProvider
     {
-        string GetHashString(Stream stream, string hashName);
         string GetHashString(byte[] buffer, string hashName);
     }
 }
